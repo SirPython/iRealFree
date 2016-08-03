c90b55e0d3bd2cd7698e48bb2c3842935dd133b8
SirPython <SirPython@users.noreply.github.com>
2016-08-02 22:15:27 -0400

    filled out tests

--- src/leadsheet/Leadsheet.spec.js
+++ src/leadsheet/Leadsheet.spec.js
@@ -1,34 +1,46 @@
 import assert from "assert";
+import teoria from "teoria";
 import Leadsheet from "./Leadsheet";
 
 describe("Leadsheet", function() {
+    var giantSteps = null;
+
+    before(function() {
+        giantSteps = Leadsheet.parse("giant_steps,john_coltrane,swing,280|Bmaj7,D7|Gmaj7,Bb7|Ebmaj7|A-7,D7|Gmaj7,Bb7|Ebmaj7,F#7|Bmaj7|F-7,Bb7|Ebmaj7|A-7,D7|Gmaj7|C#-7,F#7|Bmaj7|F-7,Bb7|Ebmaj7|C#-7,F#7");
+    });
+
     describe("static #parse()", function() {
         // TODO: Be more specific
         it("should parse and store the metadata block", function() {
-
+            assert.equal(giantSteps.metadata.title, "Giant Steps");
+            assert.equal(giantSteps.metadata.composer, "John Coltrane");
+            assert.equal(giantSteps.metadata.feel, "swing");
+            assert.equal(giantSteps.metadata.bpm, 280);
         });
         it("should parse and store single-chord measures", function() {
-
+            assert.deepEqual(giantSteps.measures[2].chords[0], teoria.chord("Ebmaj7"));
         });
         it("should parse and store multi-chord measures", function() {
-
+            assert.deepEqual(giantSteps.measures[0].chords, [teoria.chord("Bbmaj7"), teoria.chord("D7")]);
         });
         it("should parse and store multiple measures", function() {
-
+            assert(giantSteps.measures.length > 0);
         });
     });
     describe("#transpose()", function() {
-        it("should transpose all the chords up", function() {
+        var clone = null;
 
+        beforeEach(function() {
+            clone = JSON.parse(JSON.stringify(giantSteps));
+        });
+
+        it("should transpose all the chords up", function() {
+            clone.transpose("m2");
+            assert.deepEqual(clone.measures[0].chords[0], teoria.chord("Cmaj7"));
         });
         it("should transpose all the chords down", function() {
-
-        });
-        // TODO: When user is trying to transpose chart, can instead
-        // enter an instrument key (ex: Eb) to transpose to. This may
-        // not be relevant based on future UI decisions.
-        it("should transpose based on passed-in instrument key", function() {
-
+            clone.transpose("P-4");
+            assert.deepEqual(clone.measures[0].chords[0], teoria.chord("F#maj7"));
         });
     });
     describe("#save()", function() {
