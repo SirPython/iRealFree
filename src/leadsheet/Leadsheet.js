96360d60e481c2ebcfacaf30479e4152f09ccb22
SirPython <SirPython@users.noreply.github.com>
2016-08-02 22:22:56 -0400

    stuck in some blank methods for the tests

--- src/leadsheet/Leadsheet.js
+++ src/leadsheet/Leadsheet.js
@@ -16,6 +16,33 @@ class Leadsheet {
         this.metadata = metadata;
         this.measures = measures;
     }
+
+    /**
+     * @param {string} chart The description for the leadsheet chart.
+     *
+     * Format is:
+     *
+     *     [, separated metadata][| separated measures][, separated chords]
+     *
+     * Example:
+     *
+     *     "foo","bar","swing",120|C7|F7|C7|C7,F#o7
+     *
+     * @returns {Leadsheet} The parsed leadsheet chart.
+     */
+    static parse(chart) {
+        return new Leadsheet(new Leadsheet.Metadata(), [new Leadsheet.Measure()]);
+    }
+
+    /**
+     * Transposes all the chords in the chart the distance specified by the
+     * interval.
+     *
+     * @param {string} interval The interval to transpose.
+     */
+    transpose(interval) {
+
+    }
 }
 
 Leadsheet.Metadata = Metadata;
