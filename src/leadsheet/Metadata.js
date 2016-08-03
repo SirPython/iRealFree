d6da28561f3a021199f24f84962d88f77e4ae94a
SirPython <SirPython@users.noreply.github.com>
2016-08-02 22:15:10 -0400

    added bpm to metadata

--- src/leadsheet/Metadata.js
+++ src/leadsheet/Metadata.js
@@ -6,12 +6,14 @@ export default class Metadata {
      * @param {string} title The title of the chart.
      * @param {string} composer The composer of the chart.
      * @param {string} feel The feel of the chart (swing, up, bossa, funk)
+     * @param {string} bpm The BPM that the chart should be played.
      *
      * TODO: Add more metadata stuffs.
      */
-    constructor(title, composer, feel) {
+    constructor(title, composer, feel, bpm) {
         this.title = title;
         this.composer = composer;
         this.feel = feel;
+        this.feel = parseInt(bpm, 10);
     }
 }
