1f526164e8188ba3c9c7292cbe88d1b9b1818783
SirPython <SirPython@users.noreply.github.com>
2016-08-16 22:46:52 -0400

    Vogel says these shouldn't be inner classes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import Leadsheet from "./leadsheet/Leadsheet";
+import Metadata  from "./leadsheet/Metadata";
+import Measure   from "./leadsheet/Measure";
+
+import Sampler   from "./sampler/Sampler";
+import Sample    from "./sampler/Sample";
+
+
 
 let window = window || {}; // for when testing
-window.Leadsheet = Leadsheet; //for browser-interation purposes
+window.Leadsheet = Leadsheet; //for browser-interaction purposes
 window.giantSteps = Leadsheet.parse("Giant Steps,John Coltrane,swing,280:Bmaj7,D7|Gmaj7,Bb7|Ebmaj7|A-7,D7|Gmaj7,Bb7|Ebmaj7,F#7|Bmaj7|F-7,Bb7|Ebmaj7|A-7,D7|Gmaj7|C#-7,F#7|Bmaj7|F-7,Bb7|Ebmaj7|C#-7,F#7");
--- src/leadsheet/Leadsheet.js
+++ src/leadsheet/Leadsheet.js
@@ -2,16 +2,17 @@ import teoria   from "teoria";
 import Metadata from "./Metadata";
 import Measure  from "./Measure";
 
+
 /**
  * Represents a standard jazz leadsheet one might come upon. Includes things
  * such as the chords, the feel, etc.
  */
-class Leadsheet {
+export default class Leadsheet {
     /**
      * This should only be called by the static Leadsheet#parse() method.
      *
-     * @param {Leadsheet.Metadata} metadata The metadata of the chart.
-     * @param {Leadsheet.Chord[]} chords The measures of the chart.
+     * @param {Metadata} metadata The metadata of the chart.
+     * @param {Chord[]} chords The measures of the chart.
      */
     constructor(metadata, measures) {
         this.metadata = metadata;
@@ -36,11 +37,11 @@ class Leadsheet {
         [metadata, measures] = chart.split(":");
 
         /* Parse the metadata. */
-        metadata = new Leadsheet.Metadata(...metadata.split(",")); // TODO: This is such a beautiful solution, but it doesn't change BPM to number... Metadata does that. Is that okay?
+        metadata = new Metadata(...metadata.split(",")); // TODO: This is such a beautiful solution, but it doesn't change BPM to number... Metadata does that. Is that okay?
 
         /* Parse the measures */
         measures = measures.split("|").map( // each measure
-            measure => new Leadsheet.Measure(measure.split(",").map( // each chord
+            measure => new Measure(measure.split(",").map( // each chord
                 chord => teoria.chord(chord) // turn chord into teoria Chord
             ))
         );
@@ -62,8 +63,3 @@ class Leadsheet {
         }
     }
 }
-
-Leadsheet.Metadata = Metadata;
-Leadsheet.Measure  = Measure;
-
-export default Leadsheet;
