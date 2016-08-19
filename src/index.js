352c8a70111e62b94680b0f464bdd7777485db39
SirPython <SirPython@users.noreply.github.com>
2016-08-18 20:37:53 -0400

    I'm done writing tests for right now... wasting too much time

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import Measure   from "./leadsheet/Measure";
 import Sampler   from "./sampler/Sampler";
 import Sample    from "./sampler/Sample";
 
-
 window.Leadsheet = Leadsheet; //for browser-interaction purposes
 window.giantSteps = Leadsheet.parse("Giant Steps,John Coltrane,swing,280:Bmaj7,D7|Gmaj7,Bb7|Ebmaj7|A-7,D7|Gmaj7,Bb7|Ebmaj7,F#7|Bmaj7|F-7,Bb7|Ebmaj7|A-7,D7|Gmaj7|C#-7,F#7|Bmaj7|F-7,Bb7|Ebmaj7|C#-7,F#7");
