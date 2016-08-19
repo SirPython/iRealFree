5804f17b37363eb00558287169993071a6eb26fc
SirPython <SirPython@users.noreply.github.com>
2016-08-18 23:09:15 -0400

    started trying to get a sound produced, but realized that I still need to get an AUDIO buffer from my ARRAY buffer

--- src/sampler/Sampler.js
+++ src/sampler/Sampler.js
@@ -17,6 +17,8 @@ export default class Sampler {
     constructor(samples) {
         this.samples = samples;
         this.ctx = new AudioContext() || new webkitAudioContext() || null;
+
+        this._runningSounds = {};
     }
 
     /**
@@ -26,23 +28,45 @@ export default class Sampler {
      * @param {string} instrument The instrument sample to use.
      * @param {number} [duration] The duration (ms) to play the note.
      *
-     * If duration is left out, a unique ID is returned that can be passed into
-     * stop() to have the note stop playing.
+     * If duration is left out, the unique ID returned can be passed into stop()
+     * to have the note stop playing.
      *
      * @returns {Symbol} The unique ID to the sound being played for stop()
      */
     play(note, instrument, duration) {
+        let sample = this.samples[instrument];
 
+        let src = this.ctx.createBufferSource();
+        src.buffer = sample.sample;
+        src.loop = true;
+
+        // TODO: modify playback speed here
+
+        src.connect(this.ctx.destination);
+        if(duration) {
+            src.start(0, 0, duration);
+        } else {
+            src.start();
+
+            let symbol = Symbol(`${note} ${instrument} ${duration}`);
+            this._runningSounds[symbol] = src;
+            return symbol; // the unique id
+        }
     }
 
     /**
-     * Stops a continuous sound being played. If play() is called without a
+     * Stops a running sound being played. If play() is called without a
      * duration parameter, an ID is returned that can be given to this
      * to stop the sound from playing.
      *
-     * @param {Symbol} id The id of the continuous sound.
+     * @param {Symbol} id The id of the running sound.
      */
     stop(id) {
+        if(!(id in this._runningSounds)) {
+            throw new Error("ID not found in running sounds.");
+        }
 
+        this._runningSounds[id].stop();
+        delete this._runningSounds[id];
     }
 }
