a42cdc32548cff6731d876d84f8f7b1868b7c71f
SirPython <SirPython@users.noreply.github.com>
2016-08-18 22:23:04 -0400

    going to store audio as base64 string

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import Measure   from "./leadsheet/Measure";
 
 import Sampler   from "./sampler/Sampler";
 import Sample    from "./sampler/Sample";
+import samples   from "./samples";
 
 window.Leadsheet = Leadsheet; //for browser-interaction purposes
+window.Metadata  = Metadata;
+window.Measure   = Measure;
+window.Sampler   = Sampler;
+window.Sample    = Sample;
+
 window.giantSteps = Leadsheet.parse("Giant Steps,John Coltrane,swing,280:Bmaj7,D7|Gmaj7,Bb7|Ebmaj7|A-7,D7|Gmaj7,Bb7|Ebmaj7,F#7|Bmaj7|F-7,Bb7|Ebmaj7|A-7,D7|Gmaj7|C#-7,F#7|Bmaj7|F-7,Bb7|Ebmaj7|C#-7,F#7");
+window.samples = samples;
