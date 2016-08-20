25f899dc49ae18c4f07ea1804c6a273b676f630b
SirPython <SirPython@users.noreply.github.com>
2016-08-19 22:56:29 -0400

    added meter and phrase to metadata

--- src/leadsheet/Metadata.js
+++ src/leadsheet/Metadata.js
@@ -7,11 +7,15 @@ export default class Metadata {
      * @param {string} composer The composer of the chart.
      * @param {string} feel The feel of the chart (swing, up, bossa, funk)
      * @param {string} bpm The BPM that the chart should be played.
+     * @param {string} meter The meter of the chart (as in, 4/4, 5/4, etc).
+     * @param {string} phrase The number of bars in a phrase.
      */
-    constructor(title, composer, feel, bpm) {
+    constructor(title, composer, feel, bpm, meter, phrase) {
         this.title = title;
         this.composer = composer;
         this.feel = feel;
         this.bpm = parseInt(bpm, 10);
+        this.meter = meter;
+        this.phrase = parseInt(phrase, 10);
     }
 }
