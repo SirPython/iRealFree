4e6472e453bfb6afd6efe6ef3631d829e8506e1d
SirPython <SirPython@users.noreply.github.com>
2016-08-22 19:52:31 -0400

    tried using blobs to make array buffer, no good

--- src/sampler/Sample.js
+++ src/sampler/Sample.js
@@ -4,11 +4,20 @@ import "regenerator-runtime/runtime";
  * Utility method: Converts a base64 string into an ArrayBuffer.
  *
  * @param {string} base64 A base64-encoded string.
+ * @param {string} mime The mime type of the data in the base64 string.
+ *
+ * @returns {ArrayBuffer} An array buffer.
  *
  * It's so beautiful.
  */
-const base64ToArrayBuffer = base64 =>
-    new TextEncoder("utf8").encode(atob(base64)).buffer;
+const base64ToArrayBuffer = (base64, mime) => {
+    //new TextEncoder("utf8").encode(atob(base64)).buffer;
+    let fr = new FileReader();
+    fr.readAsArrayBuffer(new Blob([atob(base64)], {type: mime}));
+    return new Promise(
+        resolve => fr.onloadend = () => resolve(fr.result)
+    );
+}
 
 /**
  * Represents a single sample to be used in a sampler.
@@ -33,12 +42,9 @@ export default class Sample {
      * @param {AudioContext} ctx The audio context of the sampler.
      */
     async loadAudio(ctx) {
-        debugger;
-        var buffer = base64ToArrayBuffer(this.sampleData);
+        var buffer = await base64ToArrayBuffer(this.sampleData, "audio/x-wav");
         debugger;
         var audio  = await ctx.decodeAudioData(buffer);
         this.audio = audio;
-
-        debugger;
     }
 }
