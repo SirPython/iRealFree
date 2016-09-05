267ff9be017c0be5a16f67d66b1020f264524fe3
SirPython <SirPython@users.noreply.github.com>
2016-09-05 19:40:40 -0400

    removed all stop() related code

--- src/sampler/Sampler.js
+++ src/sampler/Sampler.js
@@ -14,10 +14,8 @@ export default class Sampler {
     constructor(samples) {
         this.samples = samples;
         this.ctx = new AudioContext() || new webkitAudioContext() || null;
-        //this._runningSounds = {};
 
         /* Load each samples audio buffer. */
-
         for(let sample in this.samples) {
             this.samples[sample].loadAudio(this.ctx);
         }
@@ -29,11 +27,6 @@ export default class Sampler {
      * @param {teoria Note | teoria Note[]} notes The notes to play.
      * @param {string} instrument The instrument sample to use.
      * @param {number} [duration] The duration (s) to play the note.
-     *
-     * A unique ID returned can be passed into stop() to have the note stop
-     * playing.
-     *
-     * @returns {Symbol} The unique ID to the sound being played for stop()
      */
     play(notes, instrument, duration) {
         let sample = this.samples[instrument];
@@ -57,7 +50,7 @@ export default class Sampler {
             return src;
         }
 
-        /* This allows for both arrays and just a single note to be passed. */
+        /* This allows for both arrays AND just a single note to be passed. */
         if(Array.isArray(notes)) {
             notes.map(setupSource);
         } else {
@@ -65,46 +58,14 @@ export default class Sampler {
             notes = [setupSource(notes)];
         }
 
-        let id = Symbol(`${notes} ${instrument} ${duration}`);
-        //this._runningSounds[id] = notes;
-
         if(duration) {
             notes.forEach(
                 note => note.start(0, 0, duration)
             );
-
-            /*return new Promise(
-                resolve => setTimeout(() => {
-                    this.stop(id); // remove id from running sounds for GC
-                    resolve();     // callee is told when note has stopped
-                }, duration * 1000)
-            );*/
         } else {
             notes.forEach(
                 note => note.start()
             );
         }
-
-        /* By also returning an id even when duration is passed in, the note
-           can be stopped before it is finished playing.*/
-        //return id; // TODO wat no? u no two retunrs
     }
-
-    /**
-     * Stops a running sound being played. If play() is called without a
-     * duration parameter, an ID is returned that can be given to this
-     * to stop the sound from playing.
-     *
-     * @param {Symbol} id The id of the running sound.
-     */
-    /*stop(id) {
-        if(!(id in this._runningSounds)) {
-            throw new Error("ID not found in running sounds.");
-        }
-
-        this._runningSounds[id].forEach(
-            sound => sound.stop()
-        );
-        delete this._runningSounds[id];
-    }*/
 }
