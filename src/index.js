0bb6a84099a61a5b6a662dbb1cb92f514bb2c51a
SirPython <SirPython@users.noreply.github.com>
2016-09-11 21:41:43 -0400

    added pattern sample support; refactored out sampler, putting play() in sample

--- src/index.js
+++ src/index.js
@@ -6,22 +6,29 @@ import Leadsheet from "./leadsheet/Leadsheet";
 import Metadata  from "./leadsheet/Metadata";
 import Measure   from "./leadsheet/Measure";
 
-import Sampler   from "./sampler/Sampler";
 import Sample    from "./sampler/Sample";
 
 window.teoria    = teoria; //for browser-interaction purposes
 window.Leadsheet = Leadsheet;
 window.Metadata  = Metadata;
 window.Measure   = Measure;
-window.Sampler   = Sampler;
 window.Sample    = Sample;
 
 let giantSteps = Leadsheet.parse("Giant Steps,John Coltrane,swing,280:Bmaj7,D7|Gmaj7,Bb7|Ebmaj7|A-7,D7|Gmaj7,Bb7|Ebmaj7,F#7|Bmaj7|F-7,Bb7|Ebmaj7|A-7,D7|Gmaj7|C#-7,F#7|Bmaj7|F-7,Bb7|Ebmaj7|C#-7,F#7");
-let sampler = new Sampler({
-    piano: new Sample("samples/sine.wav", teoria.note("c4")),
-    bass: new Sample("samples/sine.wav", teoria.note("c4")),
-    drums: new Sample("samples/sine.wav", teoria.note("c4"))
-});
+
+let ctx = new AudioContext();
+
+let samples = {
+    piano: new Sample("samples/sine.wav", teoria.note("c4"), ctx),
+    bass: new Sample("samples/sine.wav", teoria.note("c4"), ctx),
+    swing: new Sample("samples/swing.wav", 1, ctx)
+};
+
+/*
+let mySample = new Sample(Sample.loadAudio(href, ctx), teoria.note("c4"));
+mySample.play(ctx, teoria.note("c5"), 5);
+*/
 
 window.giantSteps = giantSteps;
-window.sampler = sampler;
+window.ctx = ctx;
+window.samples = samples;
--- src/sampler/Sample.js
+++ src/sampler/Sample.js
@@ -7,14 +7,16 @@ export default class Sample {
     /**
      * Creates a new Sample.
      *
-     * @param {string} href A href of the sample.
-     * @param {teoria Note} note The note being played in the audio sample.
+     * @param {string} href The href of the sample.
+     * @param {teoria Note | number} note The note being played in the audio
+     *                                    sample. Or, the amount of measures
+     *                                    the sample lasts (for pattern samples).
      */
-    constructor(href, note) {
-        this.href = href;
+    constructor(href, note, ctx) {
         this.note = note;
 
-        this.audio = null; // loadAudioBuffer fills this.
+        this.audio = null; // _load will assign this
+        this._load(href, ctx);
     }
 
     /**
@@ -27,17 +29,63 @@ export default class Sample {
      *
      * Promise galore.
      *
+     * @param {string} href The href of the sample.
      * @param {AudioContext} ctx The audio context of the sampler.
+     *
+     * This doesn't return anything because es7 async methods cannot return
+     * values (well, if you attempt it, they just return a promise). So, instead
+     * this just assigned the value to the audio property.
      */
-    async loadAudio(ctx) {
+    async _load(href, ctx) {
         await new Promise(() => {
-            fetch(this.href).then(
+            fetch(href).then(
                 response => response.arrayBuffer().then(
                     buffer => ctx.decodeAudioData(buffer).then(
                         audio => this.audio = audio
                     )
                 )
             )
+        });
+    }
+
+    /**
+     * Plays this sample.
+     *
+     * @param {teoria Note[]} note The notes to play.
+     * @param {number} duration How long to play the sample for.
+     * @param {AudioContext} ctx The audio context to play this on.
+     *
+     * For pattern samples, null the notes parameter.
+     *
+     * @TODO: The speed probably also needs to be adjusted for pattern samples;
+     * depending on the tempo, having the sample last for a certian amount
+     * of time doesn't stop it from being interrupted. Or, duration is computed
+     * based on the amount of measures <-- that.
+     */
+    play(notes, duration, ctx) {
+        /* Hacky. Allows for notes to be nulled, which is what pattern samples
+           would use because there doesn't need to be any pitch adjustment. */
+        if(!notes) {
+            notes = [0];
+        }
+        /* Allows for a single note to be passed in, rather than an array. */
+        if(!Array.isArray(notes)) {
+            notes = [notes];
+        }
+
+        notes.forEach((note) => {
+            let src = ctx.createBufferSource();
+            src.buffer = this.audio;
+
+            if(typeof note === "number") {
+                src.loop = true;
+            } else {
+                src.playbackRate.value = note.fq() / this.note.fq();
+                src.loop = true; // TODO temporary while getting better samples.
+            }
+
+            src.connect(ctx.destination);
+            src.start(0, 0, duration);
         });
     }
 }
